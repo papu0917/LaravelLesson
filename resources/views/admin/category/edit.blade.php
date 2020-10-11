bc329532f2125b15e3af8bcf4128934098357773
mitsuhiro <papu.m0917@icloud.com>
2020-10-11 17:36:20 +0900

    test

--- resources/views/admin/category/edit.blade.php
+++ resources/views/admin/category/edit.blade.php
@@ -5,12 +5,12 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>カテゴリー編集</h2>
+                <h2>カテゴリー</h2>
                 <form action="{{ action('Admin\CategoryController@update') }}" method="post" enctype="multipart/form-data">
 
                     @if (count($errors) > 0)
                         <ul>
-                            @foreach($errors->all() as $e)
+                            @foreach ($errors->all() as $e)
                                 <li>{{ $e }}</li>
                             @endforeach
                         </ul>
@@ -23,13 +23,13 @@
                     </div>
                     <div class="form-group row">
                         <div class="col-md-10"></div>
-                            <input type="hidden" name="id" value="{{ $category_form->id }}">
-                            {{ csrf_field() }}
-                            <input type="submit" class="btn btn-primary" value="更新">
-                        </div>
+                        <input type="hidden" name="id" value="{{ $category_form->id }}">
+                        {{ csrf_field() }}
+                        <input type="submit" class="btn btn-primary" value="更新">
                     </div>
-                </form>
             </div>
+            </form>
         </div>
+    </div>
     </div>
 @endsection
